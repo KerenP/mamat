3033c9f20a296eb12c4b564b7f8eacc7a1c470bd
KerenP <keren.plg@gmail.com>
2019-12-18 20:41:07 +0200

    part 2 compiled

--- HW3/dict.c
+++ HW3/dict.c
@@ -4,3 +4,84 @@
 #include "hash.h"
 #include "dict.h"
 
+#define HASH_SIZE 650
+
+typedef struct _DictElement{
+    char* word;
+    char* translation;
+} DictElement;
+
+int HashWord(char* word, int hashTableSize){
+    if (!word || hashTableSize<0){
+        return -1;
+    }
+    int word_index = ((int) (word[0]-'a')*26+strlen(word))%hashTableSize;
+    return word_index;
+}
+Result PrintEntry (pDictElement dictElement){
+    if (!dictElement){
+        return  FAIL;
+    }
+    printf("%s : %s\n", dictElement->word, dictElement->translation);
+    return SUCCESS;
+}
+CompResult CompareWords (char* word1, char* word2){
+    if (!word1 || !word2){
+        return DIFFERENT;
+    }
+    if (!strcmp(word1, word2)){
+        return SAME;
+    }
+    return DIFFERENT;
+}
+char* GetEntryKey (pDictElement dictElement){
+    if (!dictElement){
+        return NULL;
+    }
+    return  dictElement->word;
+}
+void  DestroyEntry (pDictElement dictElement){
+    if (!dictElement){
+        return;
+    }
+    free(dictElement);
+}
+pHash CreateDictionary(){
+    return HashCreate (HASH_SIZE, HashWord, PrintEntry, CompareWords, GetEntryKey, DestroyEntry);//TODO: add & or declare pointers to all functions
+}
+Result AddTranslation(pHash dict, char* word, char* translation){
+    if (!dict || !word || !translation){
+        return FAIL;
+    }
+    pDictElement newElement = (pDictElement)malloc(sizeof(DictElement));//TODO: free new_element
+    if (!newElement){
+        return FAIL;
+    }
+    newElement->word = word;
+    newElement->translation = translation;
+    return HashAdd(dict, newElement);
+}
+Result Translate (pHash dict, char* word){
+    if (!dict || !word){
+        return FAIL;
+    }
+    pDictElement wanted_element = (pDictElement)HashFind(dict, word);
+    if (!wanted_element){
+        return FAIL;
+    }
+    printf("Translation: %s : %s\n", wanted_element->word, wanted_element->translation);
+    return SUCCESS;
+}
+Result DeleteTranslation (pHash dict, char* word){
+    if (!dict || !word){
+        return FAIL;
+    }
+    return HashRemove(dict, word);
+}
+Result PrintDictionary(pHash dict){
+    if (!dict){
+        return FAIL;
+    }
+    printf("Dictionary Contents\n");
+    return HashPrint(dict);
+}
--- HW3/dict.h
+++ HW3/dict.h
@@ -7,10 +7,10 @@ int HashWord(char* word, int hashTableSize);
 Result PrintEntry (pDictElement dictElement);
 CompResult CompareWords (char* word1, char* word2);
 char* GetEntryKey (pDictElement dictElement);
-void  DestroyEntry (pDictElement element);
+void  DestroyEntry (pDictElement dictElement);
 pHash CreateDictionary();
-Result AddTranslation(pHash dict , pDictElement dictElement);
-Result Translate (pHash dict);//TODO: change to void?
+Result AddTranslation(pHash dict , char* word, char* translation);
+Result Translate (pHash dict, char* word);//TODO: change to void?
 Result DeleteTranslation (pHash dict, char* word);
 Result PrintDictionary(pHash dict);
 #endif
